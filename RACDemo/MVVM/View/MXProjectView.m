24797575608e693c3345bdd9d7e8a72cdd14e968
YISHANG <supermax932016@gmail.com>
2016-08-18 10:56:07 +0800

    commit ViewFile

---
+++ RACDemo/MVVM/View/MXProjectView.m
@@ -0,0 +1,21 @@
+//
+//  MXProjectView.m
+//  RACDemo
+//
+//  Created by YISHANG on 16/8/18.
+//  Copyright © 2016年 MAXIAO. All rights reserved.
+//
+
+#import "MXProjectView.h"
+
+@implementation MXProjectView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
